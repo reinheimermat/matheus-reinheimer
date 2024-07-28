4c7050bad44f9bab64576b9cf1e461b632d47ffe
Matheus <mreinheimer0@gmail.com>
2024-07-28 16:52:17 -0300

    chore: Create Next App

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} antialiased bg-zinc-50 dark:bg-zinc-950 text-zinc-950 dark:text-zinc-50`}>{children}</body>
     </html>
   );
 }
