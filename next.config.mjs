afac70b9f8bb6dc4c228af1f52ab4d9495d0b61b
Matheus Reinheimer <mreinheimer0@gmail.com>
2024-10-20 16:00:05 -0300

    fix: resolve bugs

--- next.config.mjs
+++ next.config.mjs
@@ -8,13 +8,6 @@ const nextConfig = {
       'github.com',
     ],
   },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.node/,
-      use: 'raw-loader',
-    })
-    return config
-  },
 }
 
 export default nextConfig
